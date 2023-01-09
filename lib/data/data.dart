fe67b46661f325cb809321194076ad42e7e622ae
NdegwaJulius <juliusndegwa2@gmail.com>
2023-01-09 10:25:00 +0300

    Cart Screen Items and Quantity Selector

--- lib/data/data.dart
+++ lib/data/data.dart
@@ -24,36 +24,36 @@ final _salmon = Food(
 // Restaurants
 final _restaurant0 = Restaurant(
   imageUrl: 'assets/images/restaurant0.jpg',
-  name: 'Restaurant 0',
-  address: '200 Main St, New York, NY',
+  name: 'Floating Island Restaurant',
+  address: 'Mombasa, KE',
   rating: 5,
   menu: [_burrito, _steak, _pasta, _ramen, _pancakes, _burger, _pizza, _salmon],
 );
 final _restaurant1 = Restaurant(
   imageUrl: 'assets/images/restaurant1.jpg',
-  name: 'Restaurant 1',
-  address: '200 Main St, New York, NY',
+  name: 'Camels Joint',
+  address: 'Old Town, Mombasa',
   rating: 4,
   menu: [_steak, _pasta, _ramen, _pancakes, _burger, _pizza],
 );
 final _restaurant2 = Restaurant(
   imageUrl: 'assets/images/restaurant2.jpg',
-  name: 'Restaurant 2',
-  address: '200 Main St, New York, NY',
+  name: 'Crave',
+  address: 'City Mall, Nyali Mombasa',
   rating: 4,
   menu: [_steak, _pasta, _pancakes, _burger, _pizza, _salmon],
 );
 final _restaurant3 = Restaurant(
   imageUrl: 'assets/images/restaurant3.jpg',
   name: 'Restaurant 3',
-  address: '200 Main St, New York, NY',
+  address: '10th St, Nyali',
   rating: 2,
   menu: [_burrito, _steak, _burger, _pizza, _salmon],
 );
 final _restaurant4 = Restaurant(
   imageUrl: 'assets/images/restaurant4.jpg',
   name: 'Restaurant 4',
-  address: '200 Main St, New York, NY',
+  address: '200 Main St, Nairobi',
   rating: 3,
   menu: [_burrito, _ramen, _pancakes, _salmon],
 );
